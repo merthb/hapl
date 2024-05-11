5b0942d0658c8bb2b9fe8273e026d815ead267b4
Merth Borbála <borka.merth14@gmail.com>
2024-05-11 12:22:02 +0200

    openFile exception handling and user communication translation to hungarian

--- src/Main.hs
+++ src/Main.hs
@@ -10,13 +10,17 @@ import System.FilePath
 import Control.Parallel.Strategies
 import Data.List
 import System.Environment
+import Control.Exception
 
 main :: IO ()
 main = do
     args <- getArgs
     case args of
         [] -> do
-            putStrLn "This is a console program, that checks plagiarism between Haskell programs by a call-graph matching algorithm. So to say, with this plagiarism checker we check if the logic behind the codes are similar, without looking at the text level of the codes."
+            putStrLn "HAPL - Haskell Plágiumellenőrző szoftver \n"
+            putStrLn "Ez egy konzolos alkalmazás, amely specifikusan Haskell nyelvben írt programok közt keresi az egyezéseket."
+            putStrLn "A programok struktúráját vizsgálja, felépíti azok függvényhívási gráfját, majd részgráf egyezéseket keresve"
+            putStrLn "adja meg az egyezési százalékot.\n"
             loop
         (file:_) -> do
             b <- System.Directory.doesFileExist file
@@ -26,53 +30,74 @@ main = do
                     case (words $ head $ lines content) of
                         [] -> do
                             codes' <- parseAllCodes' (filter System.FilePath.isValid $ tail $ lines content)
-                            putStrLn "Whole codes:"
+                            putStrLn "Programok teljes gráfjainak egyezési százalékai:\n"
                             putStrLn $ writeResAll (runOnAll codes') 1
                         _ -> do
                             codes <- parseAllCodes (filter System.FilePath.isValid $ tail $ lines content) (words $ head $ lines content)
-                            codes' <- parseAllCodes' (filter System.FilePath.isValid $ tail $ lines content)
                             resps <- runParallel codes
-                            putStrLn "Result of functions:"
+                            putStrLn "A függvényenként felépített gráfok egyezési százalékai:\n"
                             putStrLn $ writeRes resps 1 (words $ head $ lines content)
-                            putStrLn "Whole codes:"
-                            putStrLn $ writeResAll (runOnAll codes') 1
-                False -> putStrLn "Invalid parameter: file doesn't exist."
+                            putStrLn ""
+                            codes' <- parseAllCodes' (filter System.FilePath.isValid $ tail $ lines content)
+                            resps' <- runParallel codes'
+                            putStrLn "\nProgramok teljes gráfjainak egyezési százalékai:\n"
+                            putStrLn $ writeResAll resps' 1
+                False -> do
+                    putStrLn "Invalid paraméter: A megadott fájl nem létezik."
+                    exitFailure
 
 loop :: IO ()
 loop = do
     oneTimeRunner
-    putStrLn "If you want to run another plagiarism check, write 'next' and then push the ENTER button, if you want to quit, just push the ENTER button."
+    putStrLn "Válasszon az alábbi lehetőségek közül, majd írja be a választott menüpont számát!"
+    m <- getMenuItem
+    case m of
+        1 -> loop
+        2 -> exitSuccess
+
+getMenuItem :: IO Int
+getMenuItem = do
+    putStrLn "[1] - Újabb ellenőrzés futtatása"
+    putStrLn "[2] - Kilépés\n"
     x <- getLine
     case x of
-        "next" -> loop
-        _ -> exitSuccess
+        "1" -> pure 1
+        "2" -> pure 2
+        _ -> do
+            putStrLn "\nA megadott érték nem megfelelő, kérem a választott menüpont számát üsse be!"
+            getMenuItem
 
 oneTimeRunner :: IO ()
 oneTimeRunner = do
-    putStrLn "To run the plagiarism checker the program needs a text file with the following informations:"
-    putStrLn "First line must contain the function names, that have to be checked individually, separated by spaces. If empty, only whole codes are checked."
-    putStrLn "From second line on each line must contain one path to a file, that must be checked."
-    putStrLn "Give the info filepath:"
-    (fs, paths) <- filePathReader -- needs the path to the file with the infos
+    putStrLn "Az ellenőrzés futtatásához szükséges megadni egy szöveges fájl (.txt) elérési útját, amelyben az alábbi imformációk szerepelnek:"
+    putStrLn "* A fájl első sora tartalmazza a függvények nevét, amelyekre önálló ellenőrzést is futtatni kíván."
+    putStrLn "  Amennyiben nem kíván önálló ellenőrzéseket is futtatni, úgy ezt a sort hagyja üresen."
+    putStrLn "* A fájl második sorától kezdve minden sor egy ellenőrizni kívánt fájl elérési útja kell legyen.\n"
+    putStrLn "Adja meg az információkat tartalmazó szöveges fájl elérési útját:"
+    (fs, paths) <- filePathReader
     case fs of
         [] -> do
             codes' <- parseAllCodes' paths
-            putStrLn "Whole codes:"
+            putStrLn "\nProgramok teljes gráfjainak egyezési százalékai:\n"
             putStrLn $ writeResAll (runOnAll codes') 1
+            putStrLn ""
         _ -> do
             codes <- parseAllCodes paths fs
-            codes' <- parseAllCodes' paths
             resps <- runParallel codes
-            putStrLn "Result of functions:"
+            putStrLn "\nA függvényenként felépített gráfok egyezési százalékai:\n"
             putStrLn $ writeRes resps 1 fs
-            putStrLn "Whole codes:"
-            putStrLn $ writeResAll (runOnAll codes') 1
+            putStrLn ""
+            codes' <- parseAllCodes' paths
+            resps' <- runParallel codes'
+            putStrLn "\nProgramok teljes gráfjainak egyezési százalékai:\n"
+            putStrLn $ writeResAll resps' 1
+            putStrLn ""
 
 writeRes :: [Response] -> Int -> [FunName] -> String
 writeRes [] _ _ = ""
 writeRes (x:xs) n fs = writeResH x n fs ++ "\n" ++ writeRes xs (n + 1) fs where
     writeResH :: Response -> Int -> [FunName] -> String
-    writeResH (id1, id2, matches) i fs = show i ++ ". " ++ show id1 ++ " <-> \n" ++ show id2 ++ ":\n" ++ showMatches matches fs where
+    writeResH (id1, id2, matches) i fs = show i ++ ". " ++ show id1 ++ " <->\n" ++ show id2 ++ ":\n" ++ showMatches matches fs where
         showMatches :: [(String, MatchNum)] -> [FunName] -> String
         showMatches [] _ = ""
         showMatches _ [] = ""
@@ -84,7 +109,7 @@ writeResAll :: [Response] -> Int -> String
 writeResAll [] _ = ""
 writeResAll (x:xs) n = writeResH x n ++ "\n" ++ writeResAll xs (n + 1) where
     writeResH :: Response -> Int -> String
-    writeResH (id1, id2, matches) i = show i ++ ". " ++ show id1 ++ " <-> \n" ++ show id2 ++ ":\n" ++ showMatches matches where
+    writeResH (id1, id2, matches) i = show i ++ ". " ++ show id1 ++ " <->\n" ++ show id2 ++ ":\n" ++ showMatches matches where
         showMatches :: [(String, MatchNum)] -> String
         showMatches [] = ""
         showMatches ((f, num):xs) = '\t' : "match" ++ ": " ++ show num ++ "%\n" ++ showMatches xs
@@ -93,6 +118,9 @@ type FunName = String
 
 runParallel :: [Code] -> IO [Response]
 runParallel = sequence . parMap rpar runAlg . makePairs
+
+catchAny :: IO a -> (SomeException -> IO a) -> IO a
+catchAny = Control.Exception.catch
 
 runAlg :: (Code, Code) -> IO Response
 runAlg ((id1, g), (id2, h)) = do
@@ -109,7 +137,7 @@ parseCode p fs = do
 parseAllCodes :: [FilePath] -> [FunName] -> IO [Code]
 parseAllCodes [] _ = pure []
 parseAllCodes (x:xs) fs = do
-    code <- parseCode x fs
+    code <- catchAny (parseCode x fs) $ \e -> do putStrLn (x ++ ": fájl beolvasása sikertelen, a program nem kezeli."); pure Nothing
     cs <- parseAllCodes xs fs
     case code of
         Just c -> pure (c:cs)
@@ -127,7 +155,7 @@ parseCode' p = do
 parseAllCodes' :: [FilePath] -> IO [Code]
 parseAllCodes' [] = pure []
 parseAllCodes' (x:xs) = do
-    code <- parseCode' x
+    code <- catchAny (parseCode' x) $ \e -> do putStrLn (x ++ ": fájl beolvasása sikertelen, a program nem kezeli."); pure Nothing
     cs <- parseAllCodes' xs
     case code of
         Just c -> pure (c:cs)
@@ -136,13 +164,18 @@ parseAllCodes' (x:xs) = do
 parseFilePath :: IO FilePath
 parseFilePath = do
     x <- getLine
-    b <- System.Directory.doesFileExist x
-    if b 
-        then 
-            pure x
-        else 
-            do
-                putStrLn "File doesn't exist, try again:"
+    case snd (System.FilePath.splitExtension x) of
+        ".txt" -> do
+            b <- System.Directory.doesFileExist x
+            if b 
+                then 
+                    pure x
+                else 
+                    do
+                        putStrLn "\nA megadott fájl nem létezik, ellenőrizze az elérési utat, majd próbálkozzon újra:"
+                        parseFilePath
+        _ -> do
+                putStrLn "\nA megadott elrési úton nem szöveges fájl található, ellenőrizze az elérési utat, majd próbálkozzon újra:"
                 parseFilePath
 
 filePathReader :: IO ([FunName],[FilePath])
